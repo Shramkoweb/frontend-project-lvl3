bd6aa8fa190953b5753f3f069699b318a2f44435
serhii.shramko <shramko.web@yahoo.com>
2020-08-05 12:08:49 +0300

    feat: add locales

---
+++ src/locales/eng.js
@@ -0,0 +1,9 @@
+export default {
+  translation: {
+    pageTitle: 'RSS reader',
+    lead: 'Start reading RSS today! It is easy, it is nicely.',
+    feedButton: 'Add',
+    inputPlaceholder: 'RSS link',
+    exampleText: 'Example',
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,9 @@
+export default {
+  translation: {
+    pageTitle: 'RSS читалка',
+    lead: 'Начни читать RSS сегодня! Это легко и весело.',
+    feedButton: 'Добавить',
+    inputPlaceholder: 'RSS ссылка',
+    exampleText: 'Пример',
+  },
+};
